a87a07f41884ef566b0e29f6dcebd826e51c8c87
siir <chadjdreier@gmail.com>
2015-09-11 14:06:33 -0700

    Finished Lab 4.2

--- Lab 4.2/lab4.2.asm
+++ Lab 4.2/lab4.2.asm
@@ -11,9 +11,25 @@
 ExitProcess proto,dwExitCode:dword
 
 .data
+arrayW WORD 1234h, 5678h, 9ABCh, 0DEF0h
+arrayD DWORD 0h
 
 .code
 main proc
+	mov ecx, LENGTHOF arrayW
+	mov esi, OFFSET arrayW
+	mov edi, OFFSET arrayD
+
+L1:
+	mov eax, LENGTHOF arrayW
+	sub eax, ecx
+	imul eax, TYPE arrayW
+
+	mov bx, [esi+eax]
+	movzx edx, bx
+	mov [edi+eax], edx
+
+	LOOP L1
 
 	invoke ExitProcess,0
 main endp
